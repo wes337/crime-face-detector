5ec13e2c15afa34dea960fcf6c4272a57ef518e6
wes337 <wesmoses@gmail.com>
2022-12-01 21:08:51 +0200

    gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
-import basicSsl from "@vitejs/plugin-basic-ssl";
 
 export default defineConfig({
-  plugins: [solidPlugin(), basicSsl()],
+  plugins: [solidPlugin()],
   server: {
     port: 3000,
     https: true,
